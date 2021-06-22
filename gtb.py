cd04fb434225e4481007cbed29da55bfb82dcce6
Stefan Abi-Karam <stefanabikaram@gmail.com>
2021-06-22 13:53:26 -0400

    added messages and buses command functionality

--- gtb.py
+++ gtb.py
@@ -2,11 +2,15 @@ from typing import OrderedDict
 import requests
 import argparse
 
-from rich.console import Console
+from rich.console import Console, RenderGroup
 from rich.table import Table
+from rich.panel import Panel
+from rich.text import Text
+from rich.markdown import Markdown
+from rich.padding import Padding
+
 
 from pprint import pprint as pp
-
 
 
 def routes(cli_args):
@@ -37,7 +41,7 @@ def stops(cli_args):
         "https://passio3.com/www/mapGetData.php?getStops=2&json=%7B%22s0%22%3A%2276%22%2C%22sA%22%3A1%7D")
     stop_data = r.json()
     stop_data = list(stop_data["stops"].values())
-    
+
     def map_stop_data(stop):
         new_stop = OrderedDict()
         new_stop["name"] = stop["name"]
@@ -59,15 +63,76 @@ def stops(cli_args):
         table.add_row(*stop.values())
 
     console = Console()
-    console.print(table)    
+    console.print(table)
+
+
+def messages(cli_args):
+    r = requests.get(
+        "https://passio3.com/www/goServices.php?getAlertMessages=1&json=%7B%22systemSelected0%22%3A%2276%22%2C%22amount%22%3A1%2C%22routesAmount%22%3A0%7D")
+    messages_data = r.json()
+    messages_data = messages_data["msgs"]
+
+    messages_data_new = [{"title": m["name"], "html": m["html"],
+                          "date_from":m["from"], "date_to":m["to"]} for m in messages_data]
+
+    console = Console()
+    console.print(Panel(Text("Messages", justify="center", style="bold")))
+    for m in messages_data_new:
+        group = RenderGroup(
+            Text(m["title"], justify="center", style="bold"),
+            Text(m["date_from"] + " to " + m["date_to"], justify="center"),
+            Padding(Text(m["html"]), (1,0,0,0))
+            )
+        console.print(Panel(group, expand=True))
+
+    
+
+def buses(cli_args):
+    r = requests.get(
+        "https://passio3.com/www/mapGetData.php?getBuses=1&json=%7B%22s0%22%3A%2276%22%2C%22sA%22%3A1%7D")
+    buses_data = r.json()
+    buses_data = list(buses_data["buses"].values())
+    buses_data = [b[0] for b in buses_data]
+    pp(buses_data)
+
+    def map_bus_data(bus):
+        new_bus = OrderedDict()
+        new_bus["bus_name"] = bus["bus"]
+        new_bus["bus_id"] = str(bus["busId"])
+        new_bus["latitude"] = str(bus["latitude"])
+        new_bus["longitude"] = str(bus["longitude"])
+        
+        new_bus["route"] = bus["route"]
+        new_bus["route_id"] = bus["routeId"]
+        return new_bus
+    buses_data_new = list(map(map_bus_data, buses_data))
+    buses_data_new.sort(key=lambda x: (x["route"], x["bus_name"]))
+
+    table = Table(title="Buses", show_header=True)
+    for key in buses_data_new[0]:
+        table.add_column(key)
+    for bus in buses_data_new:
+        table.add_row(*bus.values())
+
+    console = Console()
+    console.print(table)
+
+def eta(cli_args):
+    ...
 
 def app(cli_args):
     if cli_args["cmd_name"] == "routes":
         routes(cli_args)
     elif cli_args["cmd_name"] == "stops":
         stops(cli_args)
+    elif cli_args["cmd_name"] == "messages":
+        messages(cli_args)
+    elif cli_args["cmd_name"] == "buses":
+        buses(cli_args)
+    elif cli_args["cmd_name"] == "eta":
+        eta(cli_args)
     else:
-        print("IDK YET")
+        print("No command selected")
 
 
 if __name__ == '__main__':
@@ -77,8 +142,11 @@ if __name__ == '__main__':
 
     parser_routes = subparsers.add_parser('routes', help='...')
 
-    parser_2 = subparsers.add_parser('stops', help='...')
-    parser_3 = subparsers.add_parser('buses', help='...')
+    parser_stops = subparsers.add_parser('stops', help='...')
+    parser_buses = subparsers.add_parser('buses', help='...')
+    parser_eta = subparsers.add_parser('eta', help='...')
+
+    parser_messages = subparsers.add_parser('messages', help='...')
 
     args = parser.parse_args()
     app(vars(args))
